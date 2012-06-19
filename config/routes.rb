1f6ef189a1f02525dd270053f41e6b0a1dbdc769
niedhui <dianhui.nie@gmail.com>
2012-06-19 15:18:48 +0800

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 RedmineApp::Application.routes.draw do
-  resource :kanban, :member => {:sync => :put} do |kanban|
-    kanban.resources :user_kanbans, :as => 'users'
-    kanban.resource :user_kanbans, :as => 'my-requests'
-    kanban.resources :assigned_kanbans, :as => 'assigned-to'
-    kanban.resource :assigned_kanbans, :as => 'my-assigned', :only => [:show]
-    kanban.resource :kanban_overviews, :as => 'overview', :only => [:show]
+  resource :kanban, :member => {:sync => :put} do
+    resources :user_kanbans, :as => 'users'
+    resource :user_kanbans, :as => 'my-requests'
+    resources :assigned_kanbans, :as => 'assigned-to'
+    resource :assigned_kanbans, :as => 'my-assigned', :only => [:show]
+    resource :kanban_overviews, :as => 'overview', :only => [:show]
   end
   resources :kanban_issues
 end
